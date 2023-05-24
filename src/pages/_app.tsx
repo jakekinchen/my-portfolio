1167a6db2bcde3cb1099569fdae306bd02fcd4d8
jakebuddy7 <65054338+jakebuddy7@users.noreply.github.com>
2023-05-23 19:22:36 -0500

    header install

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,28 @@ import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
+import Head from "next/head";
+import { OGP } from "react-ogp";
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
+    <>
+    <Head>
+        <OGP
+          url="https://jakekinchen.dev"
+          title="Jake"
+          description="The personal website of Jake, a full-stack software developer."
+          siteName="jakekinchen.dev"
+          image="https://jakekinchen.dev/banner.png"
+        />
+      </Head>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+    </>
   );
 };
 
